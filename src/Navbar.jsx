87f68ea42ce3cc78aaae14f41e07f9ef6e713e09
Masum Billah <masumbillahyusuf1621@gmail.com>
2024-02-08 20:04:59 +0600

    navbar component and footer component created

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,12 +1,12 @@
 import logo from "./assets/lws-logo-en.svg";
-import { IoMdNotificationsOutline } from "react-icons/io";
+import { IoNotificationsOutline } from "react-icons/io5";
 
 export default function Navbar() {
   return (
-    <div className="">
-      <img src={logo} alt="" className="block w-20 h-20" />
+    <div className="max-w-screen-xl mx-auto flex justify-between mt-7">
+      <img src={logo} alt="" className="block max-w-[100px] md:max-w-[165px]" />
       <span className="block">
-        <IoMdNotificationsOutline />
+        <IoNotificationsOutline className="w-7 h-7" />
       </span>
     </div>
   );
